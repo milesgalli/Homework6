5d619a0b0453e96958bad64d73ec7b206e9c0865
mgalli <galli.miles@gmail.com>
2020-03-23 18:34:31 +0800

    update

--- script.js
+++ script.js
@@ -1,200 +1,196 @@
 // Define variables
+// put var
+$(document).ready(function() {
+  var cityForm = $("#city-search");
+  var searchInput = $("#search-input");
+  var weatherBody = $("#weather-body");
+  var currentWeather = $("#current-weather");
+  var historyCard = $("#history-card");
+  var searchHistory = $("#search-history");
+  var forcastSection = $("#forecast-section");
+  var fiveDayDiv = $("#five-day");
+  var searchBtn = $(".btn-primary");
+  var errormsg = $("#error-message");
+  //listItems = $(".list-group.list-group-flush")
 
-cityForm = $("#city-search");
-searchInput = $("#search-input");
-weatherBody = $("#weather-body");
-currentWeather = $("#current-weather");
-historyCard = $("#history-card");
-searchHistory = $("#search-history");
-forcastSection = $("#forecast-section");
-fiveDayDiv = $("#five-day");
-searchBtn = $(".btn-primary");
-errormsg = $("#error-message");
+  // api key
 
-//$submitbtn= $()
+  var apiKey = "6213b1d15264822a42b98a5db46ee611";
 
-// url and api key
+  // momnet.js
 
-var apiKey = "6213b1d15264822a42b98a5db46ee611";
+  var currentDate = moment().format("dddd, MMMM Do");
 
-var currentDate = moment().format("dddd, MMMM Do");
+  // define event listeners for previous search terms and search  bar
 
-// console.log("hello");
-// step 1
-// create sumbmit event listener for the search button
+  cityForm.on("submit", function(event) {
+    event.preventDefault();
+    
+    var userInput = searchInput.val();
+ 
 
-
-cityForm.on("submit", submitListener()){
-
-weatherForcast(userInput);
-storeSearchTerms(userInput);
-weatherForcast(userInput);
-
-}
-
-
-listitems.on("click", listLIstener)
-
-
-
-cityForm.on("submit", function(event) {
-  event.preventDefault();
-});
-
-  function AP1 (){
-
-  // Need to collect the user input
-
-  var userInput = searchInput.val();
-
-  var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${userInput}+&units=imperial+&appid=${apiKey}`;
-
-  // Once the user input, need to take information and use it call the API
-  // ajax call api
-
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
-    //$weatherBody.text(JSON.stringify(response));
-
-    // console.log(response)
-
-    var name = $("<h1>").text(response.name + currentDate);
-    var temprature = $("<div>").text("Temprature:" + response.main.temp + "F");
-
-    var weatherIcon = $("<img>");
-    weatherIcon.attr(
-      "src",
-      "https://openweathermap.org/img/w/" + response.weather[0].icon + ".png"
-    );
-    weatherIcon.attr(
-      "alt",
-      response.weather[0].main + " - " + response.weather[0].description
-    );
-
-    //console.log(response.weather[0].icon);
-
-    var humidity = $("<div>").text(response.main.humidity);
-    var wind = $("<div>").text(response.wind.speed);
-
-    weatherBody.empty();
-
-    weatherBody.append(name, temprature, humidity, wind, weatherIcon);
-
-    errormsg.removeClass("hide");
-
-    cityCoords = response.coord;
-
-    uvAPI(cityCoords);
-
-    weatherForcast(userInput);
-
+    todayweatherforcast(userInput);
     storeSearchTerms(userInput);
+
+    //addlistItemToSearchHistory(userInput);
   });
 
+  function todayweatherforcast(location) {
+    // Need to collect the user input
 
+    // query URL
 
-}
+    var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${location}+&units=imperial+&appid=${apiKey}`;
 
-function weatherForcast(userInput) {
-  var queryURL2 = `https://api.openweathermap.org/data/2.5/forecast?q=${userInput}+&units=imperial+&appid=${apiKey}`;
+    // Weather forcast AJAX call
 
-  $.ajax({
-    url: queryURL2,
-    method: "GET"
-  }).then(function(response) {
-    //console.log(response);
-    //console.log(response.list);
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+      //$weatherBody.text(JSON.stringify(response));
+      // console.log(response)
+      // define variables, use reponse to add necessary data to weather body
+      var name = $("<h1>").text(response.name + " "+ currentDate);
+      var temprature = $("<div>").text(
+        "Temprature:" + " " + response.main.temp + "F"
+      );
 
-    fiveDayDiv.empty();
+      var weatherIcon = $("<img>");
+      weatherIcon.attr("src","https://openweathermap.org/img/w/" + response.weather[0].icon + ".png"
+      );
+      weatherIcon.attr("alt",response.weather[0].main + " - " + response.weather[0].description
+      );
 
-    for (let i = 0; i < response.cnt; i++) {
-      var responseRef = response.list[i];
+      //console.log(response.weather[0].icon);
 
-      //console.log(responseRef);
-      //console.log(i);
+      var humidity = $("<div>").text("Humidity:"+ " " + response.main.humidity);
+      var wind = $("<div>").text("wind Speed:"+ " " + response.wind.speed);
 
-      //get the date of this forcast
-      var responseDate = moment(responseRef.dt_txt);
-      // console.log(responseDate);
-      if (parseInt(responseDate.format("HH")) == 12) {
-        var forcastCard = $("<div>").add(
-          "card",
-          "bg-primary",
-          "col-10",
-          "col-lg-2",
-          "p-0",
-          "mx-auto",
-          "mt-3"
-        );
+      // clear weatherbody before appending new varibles to the DOM
+      weatherBody.empty();
+      // append new items to the DOM
+      weatherBody.append(name, temprature, humidity, wind, weatherIcon);
 
-        var cardBody = $("<div>").add("card-body", "text-light", "p-2");
+      errormsg.hide();
 
-        var forcastTitle = $("<h5>")
-          .add("forcast-title")
-          .text(responseDate.format("MM/DD/YYYY"));
+      // Need coordinates from weather forcast API to use the UV api
+      cityCoords = response.coord;
+      // calling necessary functions after the first event listener
+      calluvindex(cityCoords);
 
-        var forcastHumid = $("<div>").text(
-          "Humidity:" + responseRef.main.humidity + "%"
-        );
+      call5dayforcast(location);
 
-        cardBody.append(forcastCard, cardBody, forcastTitle, forcastHumid);
+    });
+  }
 
-        fiveDayDiv.append(cardBody);
+  // 5 day forcast API call
+
+  function call5dayforcast(userInput) {
+    var queryURL2 = `https://api.openweathermap.org/data/2.5/forecast?q=${userInput}+&units=imperial+&appid=${apiKey}`;
+
+    $.ajax({
+      url: queryURL2,
+      method: "GET"
+    }).then(function(response) {
+      //console.log(response);
+      //console.log(response.list);
+
+      for (let i = 0; i < response.cnt; i++) {
+        var responseRef = response.list[i];
+
+        //console.log(responseRef);
+        //console.log(i);
+
+        //get the date of this forcast
+        var responseDate = moment(responseRef.dt_txt);
+        // console.log(responseDate);
+        if (parseInt(responseDate.format("HH")) == 12) {
+          var forcastCard = $("<div>").add(
+            "card",
+            "bg-primary",
+            "col-10",
+            "col-lg-2",
+            "p-0",
+            "mx-auto",
+            "mt-3"
+          );
+
+          var cardBody = $("<div>").add("card-body", "text-light", "p-2");
+
+          var forcastTitle = $("<h5>").add("forcast-title").text(responseDate.format("MM/DD/YYYY"));
+
+          var forcastTemp = $("<div>").text("Temp:" + responseRef.main.temp + "C");
+
+          var forcastHumid = $("<div>").text("Humidity:" + responseRef.main.humidity + "%"
+          );
+
+          cardBody.append(forcastCard, cardBody,forcastTitle,forcastTemp,forcastHumid);
+
+          fiveDayDiv.append(cardBody);
+        }
       }
-    }
-  });
-}
+    });
+  }
 
-function uvAPI(cityCoords) {
-  var queryURL3 = `http://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${cityCoords.lat}&lon=${cityCoords.lon}`;
+  // UV API AJAX call
+  function calluvindex(cityCoords) {
+    var queryURL3 = `http://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${cityCoords.lat}&lon=${cityCoords.lon}`;
 
-  $.ajax({
-    url: queryURL3,
-    method: "GET"
-  }).then(function(response) {
-    // console.log(response.value);
+    $.ajax({
+      url: queryURL3,
+      method: "GET"
+    }).then(function(response) {
+      // console.log(response.value);
 
-    var uvIndex = $("<div>").text("UV Index:" + response.value);
-    weatherBody.append(uvIndex);
-  });
-}
+      var uvIndex = $("<div>").text("UV Index:" + response.value);
+      weatherBody.append(uvIndex);
+    });
+  }
 
-// create user storage for city coords + user input
+  // search term function
+  function addlistItemToSearchHistory(userInput) {
 
-// add weather icon to 5 day dislay
+    
+console.log(userInput);
 
-// fix search terms
+    var searchterms = $("<li>").text(userInput);
+    searchterms.attr("class", "search-list-item");
 
-// search History
-// store whatever user input does in local storgae
-// store to a list so a sidea bar
-function addlistItemToSearchHistory (userInput){
+    searchterms.on("click", function(event) {
+      event.preventDefault();
+      var location = $(this).text();
+      console.log(location);
 
-  var searchterms = $("<li>").text(userInput);
+      // once a click on list item it changes the weather to that city
+      todayweatherforcast(location);
+    });
 
-  searchHistory.append(searchterms);
+    searchHistory.append(searchterms);
+  }
 
-}
-
-
-userinputs = localStorage.getItem("search-term");
-userinputs = JSON.parse(userinputs);
-// when load i load the page, the previous search results are there.
-userinputs.forEach(addlistItemToSearchHistory);
-
-function storeSearchTerms(userInput) {
-  userinputs.push(userInput);
-
-  //console.log(userinputs);
-  
- addlistItemToSearchHistory (userInput); 
-
+  // save user inputs to local storage
+  userinputs = localStorage.getItem("search-term");
+  userinputs = JSON.parse(userinputs);
+  if (userinputs === null) {
+    userinputs = [];
+  }
   //console.log(userinputs);
 
-  localStorage.setItem("search-term", JSON.stringify(userinputs));
-}
+  // when page loads, the previous search results are there.
+  userinputs.forEach(addlistItemToSearchHistory);
 
-// create add event listner for list items 
+  function storeSearchTerms(userInput) {
+    userinputs.push(userInput);
 
+    //console.log(userinputs);
+
+    addlistItemToSearchHistory(userInput);
+
+    //console.log(userinputs);
+
+    localStorage.setItem("search-term", JSON.stringify(userinputs));
+  }
+
+  // create add event listner for list items
+});
